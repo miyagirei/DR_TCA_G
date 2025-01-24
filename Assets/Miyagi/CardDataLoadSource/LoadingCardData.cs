7dc9dedb697f719bec05fa3d3aa79ef4b0d4ee7a
Miyagi <ra.mu.umy243@gmail.com>
2025-01-25 03:20:53 +0900

    Adding parameter data and organizing scenes in one place

--- Assets/Miyagi/CardDataLoadSource/LoadingCardData.cs
+++ Assets/Miyagi/CardDataLoadSource/LoadingCardData.cs
@@ -1,20 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LoadingCardData : MonoBehaviour
 {
-    DataController _data_controller;
+    DataController _card_data_controller;
+    ParameterData _parameter_data;
+
     void Start() {
-        _data_controller = GameObject.Find("CardDataController").GetComponent<DataController>();
-        _data_controller.LoadingCardData();
+        _parameter_data = GameObject.Find("ParameterData").GetComponent<ParameterData>();
+        _card_data_controller = GameObject.Find("CardDataController").GetComponent<DataController>();
+        _parameter_data.LoadParameterData();
     }
 
     private void Update()
     {
-        if (_data_controller.isCompleteSave()) {
-            _data_controller.SaveCardDataList();
+        if (!_parameter_data.isLoaded()) {
+            return;
+        }
+        if (!_parameter_data.isDetermine()) {
+            return;
+        }
+
+        if (!_parameter_data.isSameVersion()) {
+            _card_data_controller.LoadingCardData();
+            _parameter_data.Saving();
+        }
+
+        if (_card_data_controller.isCompleteSave()) {
+            _card_data_controller.SaveCardDataList();
             Debug.Log("complete");
+            SceneManager.LoadScene("HomeScene");
+        }
+
+        if (_parameter_data.isSameVersion()) {
+            Debug.Log("SameVersion");
+            SceneManager.LoadScene("HomeScene");
         }
     }
 }
---
+++ Assets/Miyagi/CardDataLoadSource/ParameterData.cs
@@ -0,0 +1,129 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class ParameterData : MonoBehaviour
+{
+    DataController _data_controller;
+
+    [HideInInspector] float CPU_THINGKING_TIME = 0;
+    [HideInInspector] float SCENE_CHANGE_TIME = 0;
+    [HideInInspector] float TURN_CHANGE_TIME = 0;
+    [HideInInspector] float CARD_EFFECT_DISTANCE = 0;
+    [HideInInspector] float TRASH_DECIDED_HEIGHT = 0;
+    [HideInInspector] float TRASH_UNDECIDED_HEIGHT = 0;
+    [HideInInspector] float TIME_UNTIL_TIME_RUNS_OUT = 0;
+    [HideInInspector] float CARD_MOVING_SPEED = 2f;
+    [HideInInspector] int RESET_CARD_COUNT = 0;
+    [HideInInspector] float VERSION = 0.01f;
+
+    bool _same_version = false;
+    bool _loaded = false;
+    bool _determine = false;
+    public bool isSameVersion() => _same_version;
+    public bool isLoaded() => _loaded;
+    public bool isDetermine() => _determine;
+
+    void Start()
+    {
+        _same_version = false;
+        _loaded = false;
+        _determine = false;
+    }
+
+    public async void LoadParameterData() {
+        _data_controller = GameObject.Find("DataController").GetComponent<DataController>();
+        CPU_THINGKING_TIME = await _data_controller.GetParamValueFloat("CPU_THINGKING_TIME");
+        SCENE_CHANGE_TIME = await _data_controller.GetParamValueFloat("SCENE_CHANGE_TIME");
+        TURN_CHANGE_TIME = await _data_controller.GetParamValueFloat("TURN_CHANGE_TIME");
+        CARD_EFFECT_DISTANCE = await _data_controller.GetParamValueFloat("CARD_EFFECT_DISTANCE");
+        TRASH_DECIDED_HEIGHT = await _data_controller.GetParamValueFloat("TRASH_DECIDED_HEIGHT");
+        TRASH_UNDECIDED_HEIGHT = await _data_controller.GetParamValueFloat("TRASH_UNDECIDED_HEIGHT");
+        TIME_UNTIL_TIME_RUNS_OUT = await _data_controller.GetParamValueFloat("TIME_UNTIL_TIME_RUNS_OUT");
+        CARD_MOVING_SPEED = await _data_controller.GetParamValueFloat("CARD_MOVING_SPEED");
+        RESET_CARD_COUNT = await _data_controller.GetParamValueInt("RESET_CARD_COUNT");
+        VERSION = await _data_controller.GetParamValueFloat("VERSION");
+        _loaded = true;
+    }
+
+    void Update()
+    {
+        if (!_data_controller.isWaiting()) {
+            return;
+        }
+
+        if (GetParameterData("parameter_data") != null)
+        {
+            if (!isLoaded()) {
+                return;
+            }
+
+            if (GetParameterData("parameter_data").VERSION != VERSION) {
+                _same_version = false;
+            } else if (GetParameterData("parameter_data").VERSION == VERSION) {
+                _same_version = true;
+            }
+            _determine = true;
+        }
+        else {
+            Saving();
+        }
+    }
+
+    public Parameter GetParameterData(string json_file_name) {
+        string file_path = Path.Combine(Application.streamingAssetsPath, json_file_name + ".json");
+
+        if (File.Exists(file_path))
+        {
+            string json = File.ReadAllText(file_path);
+
+            Parameter parameter = JsonUtility.FromJson<Parameter>(json);
+            Debug.Log(file_path);
+
+            Debug.Log("Success");
+            return parameter;
+        }
+        else
+        {
+            Debug.LogError("ÉtÉ@ÉCÉãÇ™ë∂ç›ÇµÇ‹ÇπÇÒ");
+            return null;
+        }
+    }
+
+    public void Saving() {
+        SaveParameterData("parameter_data");
+    }
+
+    void SaveParameterData(string json_file_name) {
+        Parameter parameter = new Parameter();
+        parameter.CPU_THINGKING_TIME = CPU_THINGKING_TIME;
+        parameter.SCENE_CHANGE_TIME = SCENE_CHANGE_TIME;
+        parameter.TURN_CHANGE_TIME = TURN_CHANGE_TIME;
+        parameter.CARD_EFFECT_DISTANCE = CARD_EFFECT_DISTANCE;
+        parameter.TRASH_DECIDED_HEIGHT = TRASH_DECIDED_HEIGHT;
+        parameter.TRASH_UNDECIDED_HEIGHT = TRASH_UNDECIDED_HEIGHT;
+        parameter.TIME_UNTIL_TIME_RUNS_OUT = TIME_UNTIL_TIME_RUNS_OUT;
+        parameter.CARD_MOVING_SPEED = CARD_MOVING_SPEED;
+        parameter.RESET_CARD_COUNT = RESET_CARD_COUNT;
+        parameter.VERSION = VERSION;
+        
+        string json = JsonUtility.ToJson(parameter, true);
+        string file_path = Path.Combine(Application.streamingAssetsPath, json_file_name + ".json");
+        File.WriteAllText(file_path, json);
+    }
+}
+[System.Serializable]
+public class Parameter
+{
+    public float CPU_THINGKING_TIME = 0;
+    public float SCENE_CHANGE_TIME = 0;
+    public float TURN_CHANGE_TIME = 0;
+    public float CARD_EFFECT_DISTANCE = 0;
+    public float TRASH_DECIDED_HEIGHT = 0;
+    public float TRASH_UNDECIDED_HEIGHT = 0;
+    public float TIME_UNTIL_TIME_RUNS_OUT = 0;
+    public float CARD_MOVING_SPEED = 2f;
+    public int RESET_CARD_COUNT = 0;
+    public float VERSION = 0.01f;
+}
